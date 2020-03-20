cbc7f77b10752c89afa3d8dc81b45c3bcd2c2e1b
Mark Noone <manoone@tcd.ie>
2020-03-20 12:25:24 +0000

    Ui Corrections

--- ui.js
+++ ui.js
@@ -1,26 +1,99 @@
-const shBtn = document.querySelector('#show-hide-btn')
-const popup = document.getElementsByClassName('popup-layout')[0]
+const shBtn = document.querySelector('#show-hide-btn');
+const filterBtn = document.querySelector('#filter-btn');
+const filterLayout = document.querySelector('#filter-layout');
+const zoomPlusBtn = document.querySelector('#zoom-plus-btn');
+const zoomMinusBtn = document.querySelector('#zoom-minus-btn');
+const zoomLayout = document.querySelector('#zoom-layout');
+const infoBtn = document.querySelector('#info-btn');
+const infoLayout = document.querySelector('#info-layout');
+const popup = document.getElementsByClassName('popup-layout')[0];
 
-var isPopupShowing = false; 
-function updatePopup() {
+// Toggles
+const beToggle   = document.querySelector('#be-toggle');
+const dubToggle  = document.querySelector('#dub-toggle');
+const irToggle   = document.querySelector('#ir-toggle');
+const gadToggle  = document.querySelector('#gad-toggle');
+const luasToggle = document.querySelector('#luas-toggle');
+
+var uiState = {
+    isInfoShowing: false,
+    isFilterShowing: false,
+    isPopupShowing: false
+}
+
+function toggleActiveBtn(el){
+    if (el.classList.contains("active-btn")) {
+        el.classList.remove("active-btn");
+    } else {
+        el.classList.add("active-btn");
+    }
+}
+
+function toggleToggle(el){
+    if (el.classList.contains("active-toggle")) {
+        el.classList.remove("active-toggle");
+    } else {
+        el.classList.add("active-toggle");
+    }
+}
+
+function updateUI() {
     // Change button icon
-    if (isPopupShowing)
+    if (uiState.isPopupShowing) {
         shBtn.childNodes[0].className = "fas fa-angle-down";
-    else
-        shBtn.childNodes[0].className = "fas fa-angle-up"
+        infoLayout.style.top = "67vh";
+        infoBtn.style.top = "67vh";
+        zoomLayout.style.top = "59vh";
+    }
+    else {
+        shBtn.childNodes[0].className = "fas fa-angle-up";
+        infoLayout.style.top = "90vh";
+        infoBtn.style.top = "90vh";
+        zoomLayout.style.top = "82vh";
+    }
 
     // Set popup position
-    if (isPopupShowing) 
-        popup.style.bottom = "0px"
+    if (uiState.isPopupShowing) 
+        popup.style.bottom = "0px";
     else
         popup.style.bottom = "-304px";
 
+    if(uiState.isFilterShowing){
+        filterLayout.style.opacity = 1
+        setTimeout(function(){ filterLayout.style.visibility = "visible"; }, 150);
+    }
+    else{
+        filterLayout.style.opacity = 0
+        setTimeout(function(){ filterLayout.style.visibility = "hidden"; }, 150);
+    }
+
+    if(uiState.isInfoShowing){
+        infoLayout.style.opacity = 1
+        setTimeout(function(){ infoLayout.style.visibility = "visible"; }, 150);
+    }
+    else {
+        infoLayout.style.opacity = 0
+        setTimeout(function(){ infoLayout.style.visibility = "hidden"; }, 150);
+    }
 }
 
 shBtn.addEventListener('click', function(e) {
-    isPopupShowing = !isPopupShowing;
-    updatePopup();
+    uiState.isPopupShowing = !uiState.isPopupShowing;
+
+    updateUI();  // TODO: Remove and place with render call
+});
+
+filterBtn.addEventListener('click', function(e) {
+    uiState.isFilterShowing = !uiState.isFilterShowing;
+    toggleActiveBtn(filterBtn)
+    updateUI(); // TODO: Remove and place with render call
+});
+
+infoBtn.addEventListener('click', function(e) {
+    uiState.isInfoShowing = !uiState.isInfoShowing;
+    toggleActiveBtn(infoBtn)
+    updateUI(); // TODO: Remove and place with render call
 });
 
 // Start UI
-updatePopup();
+updateUI();
